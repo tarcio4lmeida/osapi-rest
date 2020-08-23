f3e5adf493468e583eaa5035a1220aa95f4e8ce5
Tárcio Almeida <tarcioalmeidalima@gmail.com>
2020-08-23 20:33:25 -0300

    Subindo projeto Spring

---
+++ src/main/java/com/restapiworks/osworks/OsapiRestApplication.java
@@ -0,0 +1,13 @@
+package com.restapiworks.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsapiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsapiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapiworks/osworks/OsapiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapiworks.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsapiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
